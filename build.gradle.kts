3734e60c96e8795778232a79a11c11ca0bd9ecc7
malaxiya2019 <40144223+malaxiya2019@users.noreply.github.com>
2026-04-20 19:09:49 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
-    id("com.android.application") version "8.0.2" apply false
+    // 将 8.0.2 升级到 8.2.2 以支持 Java 21
+    id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
