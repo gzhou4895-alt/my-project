036a0db207577b5773cec3716e6618dcfcef6956
malaxiya2019 <40144223+malaxiya2019@users.noreply.github.com>
2026-04-22 00:12:37 +0800

    Update Kotlin Android plugin version to 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // 确保 plugins 是小写
 plugins {
     id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
 
 // 这里的代码通常保持默认即可
