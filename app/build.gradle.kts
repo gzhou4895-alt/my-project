3b4f1f5df9327440aacc3a06d11739285905d81a
malaxiya2019 <40144223+malaxiya2019@users.noreply.github.com>
2026-04-21 23:57:26 +0800

    Update plugin syntax in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
+
 
 android {
     namespace "com.example.hello"
