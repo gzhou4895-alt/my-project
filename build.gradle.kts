c3d591f4abcfcb62b0e4169188515feb9cee4363
malaxiya2019 <40144223+malaxiya2019@users.noreply.github.com>
2026-04-21 23:52:29 +0800

    Update Kotlin plugin version in build.gradle.kts
    Updated Kotlin plugin version to 2.0.0 for AAR library compatibility.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+// 确保 plugins 是小写
 plugins {
     id("com.android.application") version "8.2.2" apply false
-    // 升级到 2.0.0 以兼容 AAR 库的元数据
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
+
+// 这里的代码通常保持默认即可
+
 
 
