87e96e7279e89867107834d0f43e92ab99df9b99
malaxiya2019 <40144223+malaxiya2019@users.noreply.github.com>
2026-04-22 03:35:22 +0800

    Change MainActivity to extend Activity instead of AppCompatActivity

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -1,16 +1,16 @@
 package com.example.hello
 
+import android.app.Activity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val tv = TextView(this)
-        tv.text = "LiteRT App Running OK ✅"
+        tv.text = "App Running OK ✅"
         tv.textSize = 22f
 
         setContentView(tv)
