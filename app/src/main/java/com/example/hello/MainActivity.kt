c57281980813465df21640460581cb5a3d0e51df
malaxiya2019 <40144223+malaxiya2019@users.noreply.github.com>
2026-04-24 14:56:41 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.hello
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        if (savedInstanceState == null) {
+            loadFragment(ChatFragment())
+        }
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_chat -> loadFragment(ChatFragment())
+                R.id.nav_models -> loadFragment(ModelsFragment())
+                R.id.nav_settings -> loadFragment(SettingsFragment())
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
