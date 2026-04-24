7e17178b978a353b363910aeeaec2a4e8d122181
malaxiya2019 <40144223+malaxiya2019@users.noreply.github.com>
2026-04-24 15:00:42 +0800

    Add SettingsFragment for settings screen

---
+++ app/src/main/java/com/example/hello/SettingsFragment.kt
@@ -0,0 +1,18 @@
+package com.example.hello
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(com.example.hello.R.layout.fragment_settings, container, false)
+    }
+}
